125375ec1eb3713102bc0136f580dd26a63b246c
fab48 <fab7696650@hotmail.com>
2014-05-25 18:27:40 -0500

    se adiciona la funcion de adicion por url

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 function purge_url(){
 	var url = $('#url').val();
 	var id_url =url.split("http://youtu.be/");
-
-	$('#you').html('<iframe width="500" height="500" src="http://www.youtube.com/embed/'+id_url[1]+'" frameborder="0" allowfullscreen></iframe>')
-		
+	if(id_url.length==2){
+		var x = id_url[1];
+	}
+	else{
+		id_url =url.split("https://www.youtube.com/watch?v=");
+	}
+	$('#you').html('<iframe width="500" height="500" src="http://www.youtube.com/embed/'+id_url[1]+'" frameborder="0" allowfullscreen></iframe>');
 }
